416d572776f204cc060262a487c2d522f77aedd7
minervatbd <windmillerg@gmail.com>
2025-01-26 16:21:48 -0500

    comment out optional thing

--- game/views.py
+++ game/views.py
@@ -3,7 +3,7 @@ from django.utils.timezone import datetime
 
 # Create your views here.
 def main(request):
-    print(request.build_absolute_uri()) #optional
+    # print(request.build_absolute_uri()) #optional
     return render(
         request,
         'game/main.html',
