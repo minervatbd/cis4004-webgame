338e053ea40dc87a0b3ac54daa09f7b36cce4061
minervatbd <minervatbd@gmail.com>
2025-03-14 20:22:16 -0400

    login js and html

---
+++ static/game/login.js
@@ -0,0 +1,39 @@
+let urlBase = "http://127.0.0.1:8000/api";
+
+async function login(event) {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const payload = {
+        username: username,
+        password: password,
+    }
+    
+    try {
+        let response = await fetch(`${urlBase}/login/`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(payload),
+        })
+        
+        // user not found
+        if (!response.ok) alert("Invalid username/password!");
+        else {
+            data = response.json();
+            if (data.error) {
+                    // Handle error
+                    alert(data.error);
+                } else {
+                    // Handle success
+                    alert("Login successful!");
+                    localStorage.setItem("userId", data.id);
+                    localStorage.setItem("username", data.username);
+                    // TODO redirect to new page
+                }
+        }
+    } catch(err) {console.error(err);}
+}
